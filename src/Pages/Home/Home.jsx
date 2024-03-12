72f07c106b0a601ec83577a9f11045186031271c
Nicolás García <nicolasg.ngl@gmail.com>
2024-03-12 15:21:16 -0500

    Main Components created, base routes also created with react router

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Layout from '../../Components/Layout/Layout'
 import Card from '../../Components/Card/Card'
 
  const Home = () => {
-  const [items, setItems] = useState();
+  const [items, setItems] = useState('null');
   return (
     <Layout>
       Home
